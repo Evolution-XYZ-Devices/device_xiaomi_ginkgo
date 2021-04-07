e3cac7257bc39bdf695952bb86f128d40a50a229
madmax7896 <thegreatsagar123@gmail.com>
2021-04-07 09:52:47 +0000

    ginkgo: remove_packages: Remove NgaResources

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AppDirectedSMSService Camera2 CarrierSetup ConnMO DCMO Drive Maps MyVerizonServices NgaResources OBDM_Permissions OemDmTrigger PrebuiltGmail Snap SprintDM SprintHM USCCDM VZWAPNLib VzwOmaTrigger YouTube YouTubeMusicPrebuilt obdm_stub
+LOCAL_OVERRIDES_PACKAGES := AppDirectedSMSService Camera2 CarrierSetup ConnMO DCMO Drive Maps MyVerizonServices OBDM_Permissions OemDmTrigger PrebuiltGmail Snap SprintDM SprintHM USCCDM VZWAPNLib VzwOmaTrigger YouTube YouTubeMusicPrebuilt obdm_stub
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
