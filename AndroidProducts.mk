1e520e2361ba7773d7e8a86f33d5b1ad6a68ba26
Tejas Singh <tejassingh649@rediffmail.com>
2023-04-13 09:11:45 +0000

    ginkgo: Bringup @Evolution-X OFFICIAL
    Signed-off-by: Charsi-Tejas <tejas@m.ms.evolution-x.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,11 @@
 #
-# Copyright (C) 2020 ArrowOS
+# Copyright (C) Evolution-X
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/evolution_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    evolution_ginkgo-userdebug
