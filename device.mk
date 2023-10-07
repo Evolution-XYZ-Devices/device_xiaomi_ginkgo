83eb0a5038faf634530b557e324bca3f691a4070
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-10-07 21:13:11 +0200

    ginkgo: Switch to two-stage init mounting
    * Stop handling vendor mounting from kernel, first stage init is gonna
      do that, together with system (which is now /system)
    * Use correct mount flags coming from kernel dts
    * Move system and vendor mount points to /dev/block/by-name/, we don't
      have bootdevice symlink to that point in init
    * Copy fstab to ramdisk for first stage init
    
    Change-Id: If8d75837f47c70f7ea02183bffaff233b4d6bb26
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -318,6 +318,7 @@ PRODUCT_ODM_PROPERTIES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    fstab.qcom_ramdisk \
     init.class_main.sh \
     init.ginkgo.rc \
     init.qcom.early_boot.sh \
