c46f0fffcafd02007917b0ca0d17449cf87a1f36
Bruno Martins <bgcngm@gmail.com>
2023-11-05 22:10:12 +0000

    ginkgo: Replace missing miwatermark symbol with proper libpiex_shim

--- device.mk
+++ device.mk
@@ -94,7 +94,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hidl.memory.block@1.0.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    libwatermark_shim
+    libpiex_shim
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
