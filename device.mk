c692895a9e36048a3e9f43cd145059fd25d29b2b
tejas101k <tejassingh649@rediffmail.com>
2024-03-28 08:01:55 +0000

    Revert "ginkgo: Enable updating of APEXes"
    This reverts commit f66a9e6968fee714d9144f4db80c7db10b1c7c72.

--- device.mk
+++ device.mk
@@ -27,9 +27,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.dsi.ant@1.0.vendor
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
